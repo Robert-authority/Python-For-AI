555b46b46e60ef07a5bf6ed7d2d31e3187c920d2
Robert-authority <robertus.adam20@gmail.com>
2026-01-07 05:52:24 +0700

    Update kode

--- Ai_Python.py
+++ Ai_Python.py
@@ -1,1 +1,9 @@
-print("Belajar AI dengan Python")
+def tebak_angka(n):
+    if n % 2 == 0:
+        return "Genap"
+    else:
+        return "Ganjil"
+
+angka = int(input("Masukkan angka: "))
+hasil = tebak_angka(angka)
+print(f"Angka {angka} adalah {hasil}")
